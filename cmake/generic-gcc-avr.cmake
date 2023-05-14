bfcccc2937715528b003482587e0e843c637db66
Joris Putcuyps <joris.putcuyps@gmail.com>
2023-05-14 22:30:31 +0200

    Build the hex file

--- cmake/generic-gcc-avr.cmake
+++ cmake/generic-gcc-avr.cmake
@@ -4,22 +4,21 @@ function(add_avr_executable EXECUTABLE_NAME)
         message(FATAL_ERROR "No source files given for ${EXECUTABLE_NAME}.")
     endif (NOT ARGN)
 
-    set(elf_file ${CMAKE_BINARY_DIR}/${EXECUTABLE_NAME}.elf)
-    set(hex_file ${CMAKE_BINARY_DIR}/${EXECUTABLE_NAME}.hex)
+    set(elf_file ${EXECUTABLE_NAME}.elf)
+    set(hex_file ${EXECUTABLE_NAME}.hex)
 
     add_executable(${EXECUTABLE_NAME} ${ARGN})
     set_target_properties(
             ${EXECUTABLE_NAME}
             PROPERTIES
-            RUNTIME_OUTPUT_DIRECTORY ${CMAKE_BINARY_DIR}
             SUFFIX ".elf"
     )
 
     add_custom_command(
-            OUTPUT ${hex_file}
-            COMMAND
-            ${CMAKE_OBJCOPY} -R .eeprom -R .fuse -R .lock -R .signature -R .user_signatures -O ihex ${elf_file} ${hex_file}
-            DEPENDS ${EXECUTABLE_NAME}
+      TARGET ${EXECUTABLE_NAME}
+          POST_BUILD
+          COMMAND ${CMAKE_OBJCOPY} -R .eeprom -R .fuse -R .lock -R .signature -R .user_signatures -O ihex ${elf_file} ${hex_file}
+          BYPRODUCTS ${hex_file}
     )
 
 endfunction(add_avr_executable)

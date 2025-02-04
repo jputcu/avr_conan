a08b74d2748fb30994b96da5d8e699b02ef0d388
Joris Putcuyps <joris.putcuyps@gmail.com>
2025-02-04 16:15:29 +0100

    Add libstdcpp to zakkemble package

--- toolchain/zakkemble/conanfile.py
+++ toolchain/zakkemble/conanfile.py
@@ -13,6 +13,8 @@ class ZakKembleAvrGccConan(ConanFile):
 
     def build(self):
         get(self, self.conan_data["sources"][self.version][str(self.settings.os).lower()]['url'], strip_root=True)
+        # Add a stdc++ library for AVR
+        get(self, "https://github.com/modm-io/avr-libstdcpp/archive/123a0d7.zip", destination='avr-libstdcpp', strip_root=True)
 
     def layout(self):
         basic_layout(self)
@@ -23,8 +25,9 @@ class ZakKembleAvrGccConan(ConanFile):
     def package_info(self):
         suffix = ".exe" if self.settings.os == "Windows" else ""
         toolchain = os.path.join(self.package_folder, "bin", "avr")
-        self.conf_info.update("tools.build:compiler_executables", {
-            "c": f"{toolchain}-gcc" + suffix,
-            "cpp": f"{toolchain}-g++" + suffix
+        self.conf_info.define("tools.build:compiler_executables", {
+            "c": f"{toolchain}-gcc{suffix}",
+            "cpp": f"{toolchain}-g++{suffix}"
         })
+        self.conf_info.append("tools.build:cxxflags", "-I" + os.path.join(self.package_folder, "avr-libstdcpp", "include"))
 

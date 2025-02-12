1957f4ea6c5fe2232286adf6095621c1dabd511f
Joris Putcuyps <joris.putcuyps@gmail.com>
2025-02-12 17:02:50 +0100

    download gcc prerequisites using get. Further build cleanup

--- toolchain/freestanding/conanfile.py
+++ toolchain/freestanding/conanfile.py
@@ -10,42 +10,44 @@ class AvrGccConan(ConanFile):
     settings = "os", "arch"
     no_copy_source = True
     package_type = "application"
-    gcc_src = os.path.join("src","gcc")
-    binutils_src = os.path.join("src","binutils")
-    avrlibc_src = os.path.join("src","avr-libc")
     config_fl = "--disable-doc"
     logfile = "avr-gcc_build.log"
+
+    def layout(self):
+        basic_layout(self)
 
     def _run(self, cmd):
         with open(os.path.join(self.build_folder, self.logfile), "a") as log:
             self.run(cmd, stdout=log, stderr=log)
 
     def source(self):
-        get(self, "https://ftp.gnu.org/gnu/gcc/gcc-14.2.0/gcc-14.2.0.tar.xz", destination=self.gcc_src, strip_root=True)
-        get(self, "https://ftp.gnu.org/gnu/binutils/binutils-2.44.tar.xz", destination=self.binutils_src, strip_root=True)
-        get(self, "https://github.com/avrdudes/avr-libc/releases/download/avr-libc-2_2_1-release/avr-libc-2.2.1.tar.bz2", destination=self.avrlibc_src, strip_root=True)
-        with chdir(self, self.gcc_src):
-            self.run("bash ./contrib/download_prerequisites")
-        with chdir(self, self.avrlibc_src):
+        get(self, "https://ftp.gnu.org/gnu/gcc/gcc-14.2.0/gcc-14.2.0.tar.xz", destination="gcc", strip_root=True)
+        with chdir(self, "gcc"):
+            # self.run("bash ./contrib/download_prerequisites")
+            get(self, "http://gcc.gnu.org/pub/gcc/infrastructure/gettext-0.22.tar.gz", destination="gettext", strip_root=True)
+            get(self, "http://gcc.gnu.org/pub/gcc/infrastructure/gmp-6.2.1.tar.bz2", destination="gmp", strip_root=True)
+            get(self, "http://gcc.gnu.org/pub/gcc/infrastructure/mpfr-4.1.0.tar.bz2", destination="mpfr", strip_root=True)
+            get(self, "http://gcc.gnu.org/pub/gcc/infrastructure/mpc-1.2.1.tar.gz", destination="mpc", strip_root=True)
+            get(self, "http://gcc.gnu.org/pub/gcc/infrastructure/isl-0.24.tar.bz2", destination="isl", strip_root=True)
+        get(self, "https://ftp.gnu.org/gnu/binutils/binutils-2.44.tar.xz", destination="binutils", strip_root=True)
+        get(self, "https://github.com/avrdudes/avr-libc/releases/download/avr-libc-2_2_1-release/avr-libc-2.2.1.tar.bz2", destination="avr-libc", strip_root=True)
+        with chdir(self, "avr-libc"):
             self.run("bash ./bootstrap")
-
-    def layout(self):
-        basic_layout(self)
 
     def _build_binutils(self):
         self.output.info("Building binutils")
-        config_file = os.path.relpath(os.path.join(self.source_folder, self.binutils_src, "configure"), "binutils")
+        config_file = os.path.relpath(os.path.join(self.source_folder, "binutils", "configure"), "binutils")
         mkdir(self, "binutils")
         with chdir(self, "binutils"):
             self._run(f"bash {config_file} --prefix={self.prefix} {self.config_fl} --target=avr --disable-nls "
                     + "--disable-sim")
             self._run(f"make -j{build_jobs(self)}")
             self._run(f"make -j{build_jobs(self)} install-strip")
-        rmdir(self, "binutils")
+        #rmdir(self, "binutils")
 
     def _build_gcc(self):
         self.output.info("Building gcc 1st stage")
-        config_file = os.path.relpath(os.path.join(self.source_folder, self.gcc_src, "configure"), "gcc")
+        config_file = os.path.relpath(os.path.join(self.source_folder, "gcc", "configure"), "gcc")
         mkdir(self, "gcc")
         with chdir(self, "gcc"):
             self._run(f"bash {config_file} --prefix={self.prefix} {self.config_fl} "
@@ -56,17 +58,18 @@ class AvrGccConan(ConanFile):
 
     def _build_avrlibc(self):
         self.output.info("Building avr-libc")
-        config_file = os.path.relpath(os.path.join(self.source_folder, self.avrlibc_src, "configure"), "avr-libc")
+        rel_src_path = os.path.relpath(os.path.join(self.source_folder, "avr-libc"), "avr-libc")
+        config_file = os.path.join(rel_src_path, "configure")
         mkdir(self, "avr-libc")
         with chdir(self, "avr-libc"):
             self._run(f"bash {config_file} --prefix={self.prefix} {self.config_fl} "
-                + "--host=avr --build=\`../../{self.avrlibc_src}/config.guess\`")
+                + "--host=avr --build=\`{rel_src_path}/config.guess\`")
             self._run(f"make -j{build_jobs(self)}")
             self._run(f"make -j{build_jobs(self)} install")
 
     def _build_freestanding(self):
         self.output.info("Building gcc final stage")
-        config_file = os.path.relpath(os.path.join(self.source_folder, self.gcc_src, "configure"), "gcc")
+        config_file = os.path.relpath(os.path.join(self.source_folder, "gcc", "configure"), "gcc")
         with chdir(self, "gcc"):
             self._run(f"bash {config_file} --prefix={self.prefix} {self.config_fl} "
                 + "--target=avr --enable-languages=c,c++ --disable-nls --disable-libssp --disable-libada "
